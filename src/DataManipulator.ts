8771307c6ae706fc977091da8e09181c8d3426c8
Ryan <ryan.hunter@techelevator.com>
2023-12-15 19:31:20 -0600

    data in the row interface updated

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,29 @@
-import { ServerRespond } from './DataStreamer';
+// This file is responsible for processing the raw stock data we receive from the server
+// before the Graph component renders it.
+
+import {ServerRespond} from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+    //step four
+    //update to match the new schema.
+    price_abc: number,
+    price_def: number,
+    ratio: number,
+    timestamp: Date,
+    upper_bound: number,
+    lower_bound: number,
+    trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
-  }
+    static generateRow(serverResponds: ServerRespond[]) {
+        return serverResponds.map((el: any) => {
+            return {
+                stock: el.stock,
+                top_ask_price: el.top_ask && el.top_ask.price || 0,
+                timestamp: el.timestamp,
+            };
+        })
+    }
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import {Table, TableData} from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -57,11 +57,13 @@ class Graph extends Component<IProps, {}> {
     }
   }
 
+  //This is another component lifecycle method that gets executed
+  // whenever the component updates, i.e. when the graph gets new data.
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+          ] as unknown as TableData); // step three
     }
   }
 }
