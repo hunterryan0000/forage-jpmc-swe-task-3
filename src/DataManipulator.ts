291112e126dc0672a074e88c069bb06135ab2ce1
Ryan <ryan.hunter@techelevator.com>
2023-12-15 19:51:28 -0600

    added comments explaining code

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -13,17 +13,33 @@ export interface Row {
     upper_bound: number,
     lower_bound: number,
     trigger_alert: number | undefined,
+    //This change is necessary because it determines the structure of the object
+    // returned by the generateRow function
+    // This return object must correspond to the schema of the table in the Graph
+    // component
 }
 
 
 export class DataManipulator {
-    static generateRow(serverResponds: ServerRespond[]) {
-        return serverResponds.map((el: any) => {
-            return {
-                stock: el.stock,
-                top_ask_price: el.top_ask && el.top_ask.price || 0,
-                timestamp: el.timestamp,
-            };
-        })
+    static generateRow(serverRespond: ServerRespond[]): Row {
+        //step five
+        const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price)/2;
+        const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price)/2;
+        //we’re able to access serverRespond as an array wherein the
+        // first element is stock ABC and the second element is stock DEF
+        const ratio = priceABC/priceDEF;
+        const upperBound = 1 + 0.10;
+        const lowerBound = 1 - 0.10;
+        return {
+            //return single Row object to match table.update in Graph.tsx
+            price_abc: priceABC,
+            price_def: priceDEF,
+            ratio,
+            timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+                serverRespond[0].timestamp : serverRespond[1].timestamp,
+            upper_bound: upperBound,
+            lower_bound: lowerBound,
+            trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined
+        }
     }
 }
